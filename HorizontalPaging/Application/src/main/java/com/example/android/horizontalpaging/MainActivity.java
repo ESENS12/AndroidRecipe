4a08154c66e074b5adecd4a0bd08a01c0a7087a3
ESENS12 <ghdldid12@gmail.com>
2019-01-05 01:31:45 +0900

    ViewPager와 TabBar, Fragment 기본형 레시피

---
+++ HorizontalPaging/Application/src/main/java/com/example/android/horizontalpaging/MainActivity.java
@@ -0,0 +1,195 @@
+package com.example.android.horizontalpaging;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+/**
+ * Created by ESENS on 2019-01-05.
+ * ViewPager와 TabBar, Fragment 기본형
+ */
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    ViewPager mViewPager;
+    private final static String TAG = "ESENS::MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the UI from res/layout/activity_main.xml
+        setContentView(R.layout.sample_main);
+
+        // Set up the action bar. The navigation mode is set to NAVIGATION_MODE_TABS, which will
+        // cause the ActionBar to render a set of tabs. Note that these tabs are *not* rendered
+        // by the ViewPager; additional logic is lower in this file to synchronize the ViewPager
+        // state with the tab state. (See mViewPager.setOnPageChangeListener() and onTabSelected().)
+        // BEGIN_INCLUDE (set_navigation_mode)
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // END_INCLUDE (set_navigation_mode)
+
+        // BEGIN_INCLUDE (setup_view_pager)
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        // END_INCLUDE (setup_view_pager)
+
+        // When swiping between different sections, select the corresponding tab. We can also use
+        // ActionBar.Tab#select() to do this if we have a reference to the Tab.
+        // BEGIN_INCLUDE (page_change_listener)
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+        // END_INCLUDE (page_change_listener)
+
+        //actionBar Tab 생성
+        for (int i = 0; i < 5; i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter. Also
+            // specify this Activity object, which implements the TabListener interface, as the
+            // callback (listener) for when this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+        // END_INCLUDE (add_tabs)
+    }
+
+    // BEGIN_INCLUDE (on_tab_selected)
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, tell the ViewPager to switch to the corresponding page.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+    // END_INCLUDE (on_tab_selected)
+
+    /**
+     * Unused. Required for {@link android.app.ActionBar.TabListener}.
+     */
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * Unused. Required for {@link android.app.ActionBar.TabListener}.
+     */
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    // BEGIN_INCLUDE (fragment_pager_adapter)
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages. This provides the data for the {@link ViewPager}.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    // END_INCLUDE (fragment_pager_adapter)
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        // BEGIN_INCLUDE (fragment_pager_adapter_getitem)
+        /**
+         * Get fragment corresponding to a specific position. This will be used to populate the
+         * contents of the {@link ViewPager}.
+         *
+         * @param position Position to fetch fragment for.
+         * @return Fragment for specified position.
+         */
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a DummySectionFragment (defined as a static inner class
+            // below) with the page number as its lone argument.
+            Fragment fragment = new DummySectionFragment();
+            Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        //page의 갯수. 동적으로 구현하려면 최대값을 넣어두고 사용하는게 좋음.
+        //// TODO: 2019-01-05 내부 로직 확인 필요, getCount는 페이지 생성,이동 시에 호출되는데 어떻게 사용되고 왜 필요한지 알아둬야 함
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        /**
+         * Get title for each of the pages. This will be displayed on each of the tabs.
+         *
+         * @param position Page to fetch title for.
+         * @return Title for specified page.
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            position += 1;
+            return getString(R.string.title_section1).toUpperCase(l) + position;
+        }
+        // END_INCLUDE (fragment_pager_adapter_getpagetitle)
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply displays dummy text.
+     * This would be replaced with your application's content.
+     */
+    public static class DummySectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public DummySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+            final String page_tv_string = Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER));
+
+            final TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(page_tv_string);
+
+            //fragment에 포함된 view는 onCreateView 때 생성되기 때문에, onCreateView 에서 핸들링 해야 함
+            Button btn_add = rootView.findViewById(R.id.btn_add);
+            btn_add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Log.d(TAG,"Click PageNum : " + page_tv_string);
+                }
+            });
+            return rootView;
+        }
+    }
+
+}
