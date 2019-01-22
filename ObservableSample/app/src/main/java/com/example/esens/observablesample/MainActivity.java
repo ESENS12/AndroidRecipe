8c6c478edc5ac0e718a2be1b89f7d778ef8f4531
이승준 <sjlee@fatoscorp.com>
2019-01-22 09:49:43 +0900

    Simple Observable with RXAndroid

--- ObservableSample/app/src/main/java/com/example/esens/observablesample/MainActivity.java
+++ ObservableSample/app/src/main/java/com/example/esens/observablesample/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.esens.observablesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import rx.Observable;
 import rx.Subscriber;
@@ -17,9 +21,38 @@ public class MainActivity extends AppCompatActivity {
         Observable<String> simpleObservable = Observable.create(new Observable.OnSubscribe<String>(){
             @Override
             public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("hello this is rxJava");
+                subscriber.onNext("Message From Observer");
                 subscriber.onCompleted();
             }
         });
+
+        simpleObservable.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG,"onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG,"Error ! " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(String content) {
+                Log.d(TAG,"onNext");
+                ((TextView) findViewById(R.id.tv_content)).setText(content);
+            }
+        });
+
+        //OnError, onCompleted 등을 전부 Override 하지 않아도 된다.
+        /*simpleObservable
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String text) {
+                        ((TextView) findViewById(R.id.textView)).setText(text);
+                    }
+                });*/
+
+
     }
 }
