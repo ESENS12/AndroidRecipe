5f6263359a235953fe7af56f00583015993d03e9
이승준 <ghdldid12@gmail.com>
2019-02-28 12:37:52 +0900

    Init Firebase Analytics (not included json file)

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.firebasetest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "TEST_ID");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "TEST_NAME");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "TEST_CONTENT");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 }
