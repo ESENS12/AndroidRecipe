24bfe174fa934228776c6e4d64e87a56ff5db9b0
이승준 <ghdldid12@gmail.com>
2019-03-04 10:12:30 +0900

    Added app-id(in string.xml), crush code

--- FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -2,22 +2,56 @@ package com.example.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
-
+    String null_string = null;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        textView = (TextView)findViewById(R.id.tv_null);
 
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "TEST_ID");
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "TEST_NAME");
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "TEST_CONTENT");
+        mFirebaseAnalytics.setAnalyticsCollectionEnabled(true);
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
+        mFirebaseAnalytics.setCurrentScreen(this,"firstActivity","not changed");
+        final Button btn_crush = (Button)findViewById(R.id.btn_crush);
+
+        btn_crush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stackOverflow();
+                //btn_crush.setText(textView.getText());
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "CRUSH_ID");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "CRUSH_NAME");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "CRUSH_CONTENT");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+            }
+        });
+
+
+        //FirebaseAnalytics.getInstance()
     }
+
+    public void stackOverflow() {
+        this.stackOverflow();
+    }
+
 }
