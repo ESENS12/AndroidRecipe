cea162452db75f271ca6e68381099266315010e5
이승준 <ghdldid12@gmail.com>
2019-03-06 11:41:32 +0900

    SeekBar with follow TextView(progress)

--- SeekbarTest/app/src/main/java/com/example/seekbartest/MainActivity.java
+++ SeekbarTest/app/src/main/java/com/example/seekbartest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.seekbartest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekbar = (SeekBar)findViewById(R.id.seekBar_luminosite);
+        final TextView tv = (TextView)findViewById(R.id.tvThumb);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                tv.setText("" + progress);
+                tv.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
