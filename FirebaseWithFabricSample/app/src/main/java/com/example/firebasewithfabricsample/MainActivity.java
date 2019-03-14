15cd5a8bd12cd700f62eefa96ebb19ac31bed701
이승준 <ghdldid12@gmail.com>
2019-03-14 10:04:18 +0900

    Crashlytics use only debuggable state

--- FirebaseWithFabricSample/app/src/main/java/com/example/firebasewithfabricsample/MainActivity.java
+++ FirebaseWithFabricSample/app/src/main/java/com/example/firebasewithfabricsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebasewithfabricsample;
 
+import android.content.pm.ApplicationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.crashlytics.android.Crashlytics;
@@ -10,9 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        boolean isDebuggable =  ( 0 != ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE ) );
+
+        if(isDebuggable){
+            //debug version.. not use Crashlytics
+        }else{
+            //release version.. start Crashlytics
+            Fabric.with(this, new Crashlytics());
+        }
+
         setContentView(R.layout.activity_main);
-        overFlow();
+        //overFlow();
     }
 
 
