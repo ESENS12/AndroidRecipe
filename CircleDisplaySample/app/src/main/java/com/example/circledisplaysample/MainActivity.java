dc5cb42472bd704ac902f9aa5d0bbbb210440255
ESENS12 <ghdldid12@gmail.com>
2019-04-08 09:59:15 +0900

    Add Simple CircleDisplay Sample

--- CircleDisplaySample/app/src/main/java/com/example/circledisplaysample/MainActivity.java
+++ CircleDisplaySample/app/src/main/java/com/example/circledisplaysample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.circledisplaysample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleDisplay cd = (CircleDisplay) findViewById(R.id.circleDisplay);
+        cd.setAnimDuration(3000);
+        cd.setValueWidthPercent(55f);
+        cd.setTextSize(36f);
+        cd.setColor(Color.BLUE);
+        cd.setDrawText(true);
+        cd.setDrawInnerCircle(true);
+        cd.setFormatDigits(1);
+        cd.setTouchEnabled(true);
+        //cd.setSelectionListener(this);
+        cd.setUnit("%");
+        cd.setStepSize(0.5f);
+        // cd.setCustomText(...); // sets a custom array of text
+        cd.showValue(75f, 100f, true);
+        //CircleDisplay cd = new CircleDisplay(this);
+
     }
 }
