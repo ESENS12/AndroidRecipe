3514f457c95ef754cd153e2e9504ce6eb4f8ff4c
ESENS12 <ghdldid12@gmail.com>
2019-04-08 09:47:29 +0900

    Add Sample PieChart

--- GraphSample/app/src/main/java/com/example/graphsample/MainActivity.java
+++ GraphSample/app/src/main/java/com/example/graphsample/MainActivity.java
@@ -3,82 +3,63 @@ package com.example.graphsample;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.MPPointF;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        PieChart pieChart = (PieChart)findViewById(R.id.chart);
-//        List<Entry> entries = new ArrayList<Entry>();
-//        entries.add(new Entry());
-//        LineDataSet dataSet = new LineDataSet(entries, "label");
-//        dataSet.setColor(getResources().getColor(R.color.colorPrimaryDark));
-
         PieChart pieChart = findViewById(R.id.chart);
 
-//        ArrayList NoOfEmp = new ArrayList();
-//
-//        NoOfEmp.add(new Entry(945f, 0));
-//        NoOfEmp.add(new Entry(1040f, 1));
-//        NoOfEmp.add(new Entry(1133f, 2));
-//        NoOfEmp.add(new Entry(1240f, 3));
-//        NoOfEmp.add(new Entry(1369f, 4));
-//        NoOfEmp.add(new Entry(1487f, 5));
-//        NoOfEmp.add(new Entry(1501f, 6));
-//        NoOfEmp.add(new Entry(1645f, 7));
-//        NoOfEmp.add(new Entry(1578f, 8));
-//        NoOfEmp.add(new Entry(1695f, 9));
-//
-//
-//        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
-//
-//        ArrayList year = new ArrayList();
-//
-//        year.add("2008");
-//        year.add("2009");
-//        year.add("2010");
-//        year.add("2011");
-//        year.add("2012");
-//        year.add("2013");
-//        year.add("2014");
-//        year.add("2015");
-//        year.add("2016");
-//        year.add("2017");
-//        //PieData data = new PieData(year, dataSet);
-//        //pieChart.setData(data);
-//        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-//        pieChart.animateXY(5000, 5000);
+        //pieChart.set
+        //description
+        pieChart.getDescription().setEnabled(false);
+        //description 컬러 안내(?)
+        pieChart.getLegend().setEnabled(false);
 
         ArrayList<PieEntry> entries = new ArrayList<>();
 
         //for (int i = 0; i < 10 ; i++) {
             //entries.add(new PieEntry((float) ((Math.random() * 3) + 5 / 5)));
-            entries.add(new PieEntry(100));
+        entries.add(new PieEntry(30));
+        entries.add(new PieEntry(70));
 
         //}
 
         PieDataSet dataSet = new PieDataSet(entries, "Election Results");
 
-        dataSet.setDrawIcons(false);
-
+        //데이터간 구분 간격
         dataSet.setSliceSpace(3f);
+
+        //graph 내부 텍스트 크기 및 색상 조정
+        dataSet.setValueTextSize(30);
+        dataSet.setValueTextColor(getResources().getColor(R.color.white));
+
+        dataSet.setColors(new int[] { R.color.white, R.color.colorAccent}, this);
+
+        dataSet.setDrawIcons(false);
         dataSet.setIconsOffset(new MPPointF(0, 40));
-        dataSet.setSelectionShift(5f);
+
+        //차트 크기
+        dataSet.setSelectionShift(10f);
+
+        //int colors [] = new int[2];
+//
+//        colors[0] = getResources().getColor(R.color.colorPrimaryDark);
+//        colors[1] = getResources().getColor(R.color.white);
+//        dataSet.setColors(colors,this);
 
         PieData data = new PieData(dataSet);
 //        data.setValueFormatter(new PercentFormatter(pieChart));
