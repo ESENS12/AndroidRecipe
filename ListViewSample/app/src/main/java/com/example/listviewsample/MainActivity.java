e1d9617a8676fae071630bad8449bbd80a7bd91a
ESENS12 <ghdldid12@gmail.com>
2019-04-09 12:23:56 +0900

    Add ListviewSample 2way(simple, using customview)

--- ListViewSample/app/src/main/java/com/example/listviewsample/MainActivity.java
+++ ListViewSample/app/src/main/java/com/example/listviewsample/MainActivity.java
@@ -2,12 +2,83 @@ package com.example.listviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //ArrayAdapter<String> adapter;
+    ListViewAdapter adapter;
+
+    int clickCounter=0;
+    private ListView mListView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+
+        otherway();
+
+        if (mListView == null) {
+            mListView = (ListView) findViewById(R.id.list);
+        }
+//
+//        adapter=new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                listItems);
+
+//        adapter=new ArrayAdapter<String>(this,
+//                R.layout.listviewitem,
+//                listItems);
+
+        //setListAdapter(adapter);
+    }
+
+    public void addItems(View v) {
+
+        adapter.addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),
+                "Clicked : " + clickCounter++);
+
+        //listItems.add("Clicked : "+clickCounter++);
+        adapter.notifyDataSetChanged();
+    }
+
+    protected ListView getListView() {
+        if (mListView == null) {
+            mListView = (ListView) findViewById(R.id.list);
+        }
+        return mListView;
+    }
+
+    protected void setListAdapter(ListAdapter adapter) {
+        getListView().setAdapter(adapter);
+    }
+
+    protected ListAdapter getListAdapter() {
+        ListAdapter adapter = getListView().getAdapter();
+        if (adapter instanceof HeaderViewListAdapter) {
+            return ((HeaderViewListAdapter)adapter).getWrappedAdapter();
+        } else {
+            return adapter;
+        }
+    }
+
+    private void otherway(){
+        ListView listview ;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.list);
+        listview.setAdapter(adapter);
     }
 }
