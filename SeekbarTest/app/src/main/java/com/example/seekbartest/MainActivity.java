6cdb1967bdf8e64d4a428d4fb9d5c1487b11fe18
ESENS12 <ghdldid12@gmail.com>
2019-04-18 14:16:30 +0900

    Add Croller

--- SeekbarTest/app/src/main/java/com/example/seekbartest/MainActivity.java
+++ SeekbarTest/app/src/main/java/com/example/seekbartest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.seekbartest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.sdsmdg.harjot.crollerTest.Croller;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
                 int xPos = (seekBar.getWidth()-padding) * seekBar.getProgress() / seekBar.getMax() + sPos - (tv.getWidth()/2);
 
                 tv.setX(xPos);
+//
+//                Croller croller = (Croller) findViewById(R.id.croller);
+//                croller.setIndicatorWidth(10);
+//                croller.setBackCircleColor(Color.parseColor("#EDEDED"));
+//                croller.setMainCircleColor(Color.WHITE);
+//                croller.setMax(50);
+//                croller.setStartOffset(45);
+//                croller.setIsContinuous(false);
+//                croller.setLabelColor(Color.BLACK);
+//                croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
+//                croller.setIndicatorColor(Color.parseColor("#0B3C49"));
+//                croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
+
             }
 
             @Override
