8c93edf3ff8a1ff977acab3758fa50e71e903432
ESENS12 <ghdldid12@gmail.com>
2019-06-13 17:25:05 +0900

    Simple Spinner

--- DropDownSpinnerSample/app/src/main/java/kr/fatos/tnavi/dropdownspinnersample/MainActivity.java
+++ DropDownSpinnerSample/app/src/main/java/kr/fatos/tnavi/dropdownspinnersample/MainActivity.java
@@ -2,12 +2,51 @@ package kr.fatos.tnavi.dropdownspinnersample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private String[] arr_data;
+    private TextView tv_item;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arr_data = getResources().getStringArray(R.array.planets_array);
+        tv_item = (TextView)findViewById(R.id.tv_item);
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.planets_array,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        if(position < arr_data.length && position > -1){
+            tv_item.setText(arr_data[position]);
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
