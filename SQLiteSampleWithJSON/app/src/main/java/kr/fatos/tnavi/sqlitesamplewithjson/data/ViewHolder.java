4154567980ff73858e7b30e89a1c0a0230d50fc1
ESENS12 <ghdldid12@gmail.com>
2019-06-13 10:34:44 +0900

    Create View Holder(for click event)

---
+++ SQLiteSampleWithJSON/app/src/main/java/kr/fatos/tnavi/sqlitesamplewithjson/data/ViewHolder.java
@@ -0,0 +1,13 @@
+package kr.fatos.tnavi.sqlitesamplewithjson.data;
+
+import android.widget.TextView;
+
+public class ViewHolder {
+    public TextView name;
+    public TextView contact;
+    public TextView no;
+    public int index;
+
+    public ViewHolder() {
+    }
+}
