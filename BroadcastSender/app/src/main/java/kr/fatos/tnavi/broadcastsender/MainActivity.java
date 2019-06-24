9232595eec828cfc9136c37d6cb1f7ed3c702e0e
ESENS12 <ghdldid12@gmail.com>
2019-06-24 10:34:59 +0900

    initial commit

---
+++ BroadcastSender/app/src/main/java/kr/fatos/tnavi/broadcastsender/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.fatos.tnavi.broadcastsender;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
