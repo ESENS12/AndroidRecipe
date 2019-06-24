37edb531b4bade47da68e48da354fed5005d924f
ESENS12 <ghdldid12@gmail.com>
2019-06-24 13:40:03 +0900

    Event push with start at other Application activity(intent)

--- BroadcastSender/app/src/main/java/kr/fatos/tnavi/broadcastsender/MainActivity.java
+++ BroadcastSender/app/src/main/java/kr/fatos/tnavi/broadcastsender/MainActivity.java
@@ -1,16 +1,41 @@
 package kr.fatos.tnavi.broadcastsender;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private Context mContext;
+    private final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this.getApplicationContext();
+        button = (Button)findViewById(R.id.btn_go);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewActivity(mContext,"package name(e.g .. kr.fatos.tnavi.sample)");
+            }
+        });
+    }
 
 
+    public void startNewActivity(Context context, String packageName) {
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction("android.intent.action.CUSTOMACTION");
+        Intent message = new Intent("android.intent.action.CUSTOMACTION");
+        context.startActivity(intent);
+        sendBroadcast(message);
     }
 }
